5964a87e5210b5e057d3355f78c699fadb1a1fc9
Vítor Albuquerque Brando <vitorbrando@gmail.com>
2023-02-24 01:18:24 -0300

    Inclusão do suporte ao MQTT

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const mqtt_app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.MQTT,
+    options: {
+      url: 'mqtt://mosquitto:1883',
+    },
+  });
+  mqtt_app.listen();
+
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.APP_PORT);
--- src/variables/variables.controller.ts
+++ src/variables/variables.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { VariablesService } from './variables.service';
 import { CreateVariableDto } from './dto/create-variable.dto';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('variables')
 export class VariablesController {
@@ -10,4 +11,9 @@ export class VariablesController {
   create(@Body() createVariableDto: CreateVariableDto) {
     return this.variablesService.create(createVariableDto);
   }
+
+  @MessagePattern('device')
+  getNotifications(@Payload() data) {
+    return this.variablesService.create(data);
+  }  
 }
